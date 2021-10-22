453293888d91df141ac42a0f80f4e95b53039e6e
adriannavarro99 <adriannavarro92@gmail.com>
2021-10-22 00:21:17 -0600

    first commit

---
+++ zoo1/tester.java
@@ -0,0 +1,19 @@
+package zoo1;
+
+public class tester {
+	public static void main(String args[]) {
+		Gorilla test= new Gorilla(100);
+		
+		test.throwSomething();
+		test.throwSomething();
+		test.throwSomething();
+		test.eatBanana();
+		test.eatBanana();
+		test.climb();
+		
+		
+		test.displayEnergy();
+		
+	}
+
+}
